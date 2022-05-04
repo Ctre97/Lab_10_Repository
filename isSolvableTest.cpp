3da9024c3794b93dcfdcdeda4696f297260f9304
Levi <lnfranklin01@gmail.com>
2022-05-04 12:12:20 -0500

    update

--- isSolvableTest.cpp
+++ isSolvableTest.cpp
@@ -1,7 +1,6 @@
-//8-Puzzle solvable check
-//Collin Trehar and Levi Franklin
-//Speed and Power Studios
-
+// 8-Puzzle solvable check
+// Collin Trehar and Levi Franklin
+// Speed and Power Studios
 
 #include <iostream>
 #include <cmath>
@@ -11,19 +10,20 @@
 
 using namespace std;
 
-bool checkSolvable(int solveInput[]); // prototype
+bool checkSolvable(puzzleBoard board); // prototype
 
-//will be implemented instead when we get the class object working correctly
+// will be implemented instead when we get the class object working correctly
 
-int main (int argc, char *argv[]){ //command line input
-    // int puzzleInput[18]; 
+int main(int argc, char *argv[])
+{ // command line input
+    // int puzzleInput[18];
     // int solveInput[9];
-    bool solvable; //variable for results of checkSolvable
-    deque <puzzleBoard> boardQueue;
+    bool solvable; // variable for results of checkSolvable
+    deque<puzzleBoard> boardQueue;
 
     // for (int i = 0; i < 19; i++) // puts command line arguments into an array
     // {
-    //     puzzleInput[i] = atoi (argv[i]); // converts the char to int 
+    //     puzzleInput[i] = atoi (argv[i]); // converts the char to int
     // }
 
     // for (int i = 0; i < 9; i++)
@@ -35,40 +35,30 @@ int main (int argc, char *argv[]){ //command line input
     puzzleBoard goalBoard;
 
     char *ptr = argv[1];
-    for (int i = 0; i < 3; i++){
-        for (int j = 0; j < 3; j++){
-            initialBoard.board[i][j] = (*ptr++ - '0');         }
-    }
-   
-   ptr = argv[2];
-   for (int i = 0; i < 3; i++){
-       for (int j = 0; j < 3; j++){
-           goalBoard.board[i][j] = (*ptr++ - '0');
-       }
-   }
-
-<<<<<<< HEAD
-    // firstBoard.fillTheBoard(puzzleInput);
-    initialBoard.printTheBoard(initialBoard);
-    boardQueue.push_front (initialBoard);
-=======
-    for (int i = 0; i < 9; i++)
+    for (int i = 0; i < 3; i++)
     {
-        solveInput[i] = atoi (argv[i]);
+        for (int j = 0; j < 3; j++)
+        {
+            initialBoard.board[i][j] = (*ptr++ - '0');
+        }
     }
-  
-    puzzleBoard firstBoard;
-    firstBoard.fillTheBoard(puzzleInput);
-    firstBoard.printTheBoard();
-    boardQueue.push_front(firstBoard);
->>>>>>> 40c61554f0113d9d17edb742adcb662d78c1a601
 
-    // goalBoard.createGoalBoard(puzzleInput);
+    ptr = argv[2];
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            goalBoard.board[i][j] = (*ptr++ - '0');
+        }
+    }
+
+    initialBoard.printTheBoard(initialBoard);
+    boardQueue.push_front(initialBoard);
     goalBoard.printGoalBoard(goalBoard);
-   // solvable = checkSolvable(goalBoard);
+    // solvable = checkSolvable(goalBoard);
 
-   // checkSolvable(goalBoard);
-    solvable = checkSolvable;
+    checkSolvable(goalBoard);
+    solvable = checkSolvable(initialBoard);
 
     if (solvable) // if variable returns true
     {
@@ -80,32 +70,39 @@ int main (int argc, char *argv[]){ //command line input
     }
 
     findBlank(boardQueue.front(), boardQueue);
-    
-return 0;
+
+    return 0;
 }
 
 // function to check if the problem can be solved. Can be done from original argv input
-bool checkSolvable(int solveInput[]){
+bool checkSolvable(puzzleBoard board)
+{
     bool solvable;
     int counter = 0;
 
-    for(int i = 1; i <= 9; i++) // number to be checked
+    for (int i = 0; i <= 2; i++)
     {
-        for (int j = i; j <= 9; j++) // number being checked against
-        { 
-            if(solveInput[i] > solveInput[j])
+        for (int j = 0; j <= 2; j++)
+        {
+            for (int k = 0; k <= 2; k++) // number to be checked
             {
-                counter++;
+                for (int l = 0; l <= 2; l++) // number being checked against
+                {
+                    if (board.board[i][j] > board.board[k][l])
+                    {
+                        counter++;
+                    }
+                }
             }
         }
     }
-if(counter %2 == 0) // if the division results in no remainder it is even
-{
-    solvable = true;
-}
-else // it is odd
-{
-    solvable = false;
-}
-return solvable;
-}
+            if (counter % 2 == 0) // if the division results in no remainder it is even
+            {
+                solvable = true;
+            }
+            else // it is odd
+            {
+                solvable = false;
+            }
+            return solvable;
+        }
